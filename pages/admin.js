007ab03f2b92c35f07b08269d20c1fc9087f24e3
jaydendeveloper <contact@jayden.hu>
2022-06-13 16:35:55 +0200

    bugfix

--- pages/admin.js
+++ pages/admin.js
@@ -29,7 +29,6 @@ import Editor from 'react-simple-code-editor';
 import { highlight, languages } from "prismjs/components/prism-core";
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism-duotone-space.css';
 import {app, user, auth, db} from "./fbconfig"
 
 const Admin = () => {
