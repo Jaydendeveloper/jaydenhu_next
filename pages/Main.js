04d53911c2908a33ecf2b598a7ff5a19d6628ec6
jaydendeveloper <contact@jayden.hu>
2022-06-19 10:35:54 +0200

    UI  changes

--- pages/Main.js
+++ pages/Main.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Main = () => {
         /* <h2 className="text-[35px] text-center text-[#38b4c8]">Jayden <br /><div className="text-white" > &lt; Web developer /&gt;</div></h2> */
 
@@ -7,7 +9,8 @@ const Main = () => {
             &lt;<span className="text-[#38b4c8]">/</span>&gt;
         </div> */}
         <div className="hidden md:block lg:block mt-[15px] float-left md:text-[150px] lg:text-[250px] lg:mt-[-110px] md:mt-[-40px] md:ml-[30px] lg:ml-[300px] text-white">
-            <img className="h-[400px] mainpic" src="main.png" />
+            {/* <img className="h-[400px] mainpic" src="main.png" /> */}
+            <Image height={400} width={400} src="/main.png" className="mainpic"/>
         </div>
         <div className="text-white ml-[30px] max-w-[300px] mt-[200px] md:ml-[300px] lg:ml-[780px] md:max-w-full lg:max-w-full">
             <div className="text-[50px]">Welcome to my page!
